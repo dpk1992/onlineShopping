5b60ac530d636e3e54a30c49af9daafb2cd8b3aa
Deepak kumar <dpk120@gmail.com>
2018-02-22 14:05:14 +0530

    my project is completed successfully.

---
+++ online_shopping_backend/src/main/java/com/online_shopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.online_shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
